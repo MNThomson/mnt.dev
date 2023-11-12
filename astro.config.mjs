3737f13f171b696f66c36757d7b01d9a93886fc0
Max Thomson <git@mnt.dev>
2023-11-11 16:54:50 -0800

    [Tailwind] Add tailwindcss to project
    Ok, this one is really cool

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind()],
+});
 
